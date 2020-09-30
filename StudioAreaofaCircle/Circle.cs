e2914fe450cd8f1f708511485f94ba6c11de20d6
dantehargrow <dantehargrow@gmail.com>
2020-09-29 23:24:42 -0500

    Broke the main program down into a circle class

---
+++ StudioAreaofaCircle/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioAreaofaCircle
+{
+    class Circle
+    {
+        double radius;
+        double areaOfCircle;
+        double circumference;
+        double diameter;
+        int milesPerGal;
+        double gallons;
+        
+       public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double getAreaOfCirle()
+        {
+            this.areaOfCircle = Math.PI * this.radius * this.radius;
+            return this.areaOfCircle;
+        }
+        public double getCircumference()
+        {
+            this.circumference = 2 * Math.PI * radius;
+            return this.circumference;
+        }
+        public double getDiameter()
+        {
+            this.diameter = 2 * radius;
+            return this.diameter;
+        }
+        public void setMPG(int MPG)
+        {
+            this.milesPerGal = MPG;
+        }
+        public int getMPG()
+        {
+            return this.milesPerGal;
+        }
+        public double getGallons()
+        {
+            this.gallons = this.circumference / this.milesPerGal;
+            return this.gallons;
+        }
+    }
+}
--- StudioAreaofaCircle/Program.cs
+++ StudioAreaofaCircle/Program.cs
@@ -7,15 +7,18 @@ namespace StudioAreaofaCircle
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a radius: ");
-            double radius;
-            double areaOfCirle;
-            double circumference;
-            double diameter;
-            radius = double.Parse(Console.ReadLine());
-            areaOfCirle = Math.PI*radius*radius;
-            diameter = 2 * radius;
-            circumference = 2 * Math.PI * radius;
-            Console.WriteLine($"Area of circle: {areaOfCirle}, diameter of circle: {diameter}, circumference of circle: {circumference}");
+            double radius = double.Parse(Console.ReadLine());
+            Circle c1 = new Circle(radius);
+            double area = c1.getAreaOfCirle();
+            double circumference = c1.getCircumference();
+            double diameter = c1.getDiameter();
+            Console.WriteLine("Enter the miles per gallons for your car");
+            c1.setMPG(int.Parse(Console.ReadLine()));
+            Console.WriteLine(c1.getMPG());
+            double gallons = c1.getGallons();
+
+            Console.WriteLine($"\nArea of circle: {area}, diameter of circle: {diameter}, circumference of circle: {circumference}\n");
+            Console.WriteLine($"You'll use {gallons} gallons to cover {circumference} miles");
         }
     }
 }
