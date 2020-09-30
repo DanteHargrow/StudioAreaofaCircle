39e965fb2c7fa16499dcb8749db2cff4346f0c55
dantehargrow <dantehargrow@gmail.com>
2020-09-29 23:26:29 -0500

    Refactor

--- StudioAreaofaCircle/Program.cs
+++ StudioAreaofaCircle/Program.cs
@@ -8,12 +8,16 @@ namespace StudioAreaofaCircle
         {
             Console.WriteLine("Enter a radius: ");
             double radius = double.Parse(Console.ReadLine());
+
             Circle c1 = new Circle(radius);
             double area = c1.getAreaOfCirle();
+
             double circumference = c1.getCircumference();
             double diameter = c1.getDiameter();
+
             Console.WriteLine("Enter the miles per gallons for your car");
             c1.setMPG(int.Parse(Console.ReadLine()));
+
             Console.WriteLine(c1.getMPG());
             double gallons = c1.getGallons();
 
