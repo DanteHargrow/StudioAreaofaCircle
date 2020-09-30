3331ca2118435ecfd30c0e4a2b70297bc7e6dfa5
Dante Hargrow <dantehargrow@gmail.com>
2020-09-29 19:18:50 -0500

    Add project files.

---
+++ StudioAreaofaCircle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StudioAreaofaCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius: ");
+            double radius;
+            double areaOfCirle;
+            double circumference;
+            double diameter;
+            radius = double.Parse(Console.ReadLine());
+            areaOfCirle = Math.PI*radius*radius;
+            diameter = 2 * radius;
+            circumference = 2 * Math.PI * radius;
+            Console.WriteLine($"Area of circle: {areaOfCirle}, diameter of circle: {diameter}, circumference of circle: {circumference}");
+        }
+    }
+}
